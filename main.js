c92f20838482fe4ad7560d29d3622f1364582632
Phillip Irwin <irwin.phillip@gmail.coom>
2026-06-21 19:51:18 -0500

    adding functions

--- main.js
+++ main.js
@@ -1,5 +1,59 @@
 "use strict";
+// 1. Get deposit
+// 2. Get number of lines to bet on
+// 2. Get bet per line
 Object.defineProperty(exports, "__esModule", { value: true });
-const message = "Hello World";
-console.log(message);
+const prompt = require('prompt-sync')();
+const ROWS = 3;
+const COLS = 3;
+//function for getting deposits
+const getDeposit = () => {
+    while (true) {
+        const deposit = prompt("How much would you like to deposit: ");
+        if (deposit === null)
+            continue;
+        const depositNum = parseFloat(deposit);
+        if (Number.isNaN(depositNum) || depositNum < 0) {
+            console.log("Please input a valid deposit.");
+            continue;
+        }
+        balance += depositNum;
+        return depositNum;
+    }
+};
+//function for getting Rows
+const getRows = () => {
+    while (true) {
+        const rows = prompt("How many rows are you betting (1-3)?: ");
+        if (rows === null)
+            continue;
+        const rowsNum = parseFloat(rows);
+        if (Number.isNaN(rowsNum) || rowsNum < 0 || rowsNum > 3) {
+            console.log("Please input a valid deposit.");
+            continue;
+        }
+        return rowsNum;
+    }
+};
+//function for getting Rows
+const getBet = () => {
+    while (true) {
+        const bet = prompt("How much are you betting per line?: ");
+        if (bet === null)
+            continue;
+        const betNum = parseFloat(bet);
+        if (Number.isNaN(betNum) || betNum < 0) {
+            console.log("Please input a valid deposit.");
+            continue;
+        }
+        return betNum;
+    }
+};
+let balance = 0;
+const deposit = getDeposit();
+const rows = getRows();
+const bet = getBet();
+console.log(`You deposited ${deposit}`);
+console.log(`You are betting ${bet} on ${rows} rows`);
+console.log(`You have a balance of ${balance}`);
 //# sourceMappingURL=main.js.map
--- main.ts
+++ main.ts
@@ -1,3 +1,93 @@
-const message: string = "Hello World";
+// 1. Get deposit
+// 2. Get number of lines to bet on
+// 2. Get bet per line
 
-console.log(message);
+
+
+const prompt = require('prompt-sync')();
+
+const ROWS: number = 3;
+const COLS: number = 3;
+
+const SYMBOLS: { A: number, B: number, C: number, D: number} =  {
+    A : 3,
+    B : 4,
+    C : 5,
+    D : 8
+}
+
+//function for getting deposits
+const getDeposit = (): number => {
+    while(true) {
+        const deposit: string = prompt("How much would you like to deposit: ");
+        
+        if (deposit === null) continue;
+
+        const depositNum: number = parseFloat(deposit)
+
+
+        if (Number.isNaN(depositNum) || depositNum < 0) {
+            console.log("Please input a valid deposit.");
+            continue;
+        } 
+
+        balance += depositNum;
+        return depositNum; 
+    }  
+    
+    
+}
+
+//function for getting Rows
+const getRows = (): number => {
+    while(true) {
+        const rows: string = prompt("How many rows are you betting (1-3)?: ");
+
+        if (rows === null) continue;
+
+        const rowsNum: number = parseFloat(rows)
+
+
+        if (Number.isNaN(rowsNum) || rowsNum < 0 || rowsNum > 3)  {
+            console.log("Please input a valid deposit.");
+            continue;
+        } 
+            
+        return rowsNum; 
+        
+    }  
+    
+    
+}
+
+//function for getting Rows
+const getBet = (): number => {
+    while(true) {
+        const bet: string = prompt("How much are you betting per line?: ");
+
+        if (bet === null) continue;
+
+        const betNum: number = parseFloat(bet)
+
+
+        if (Number.isNaN(betNum) || betNum < 0)  {
+            console.log("Please input a valid deposit.");
+            continue;
+        } 
+            
+        return betNum; 
+        
+    }  
+    
+    
+}
+
+let balance: number = 0;
+
+const deposit: number = getDeposit();
+const rows: number = getRows();
+const bet: number = getBet();
+
+console.log(`You deposited ${deposit}`);
+console.log(`You are betting ${bet} on ${rows} rows`);
+console.log (`You have a balance of ${balance}`)
