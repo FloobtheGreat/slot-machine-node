bc3d897e537cb28519bfd4e8e111d37fcbecc80d
Phillip Irwin <irwin.phillip@gmail.coom>
2026-06-22 00:32:40 -0500

    Generating Slot spins added

--- main.ts
+++ main.ts
@@ -9,12 +9,19 @@ const prompt = require('prompt-sync')();
 const ROWS: number = 3;
 const COLS: number = 3;
 
-const SYMBOL_COUNTS: { A: number, B: number, C: number, D: number} =  {
-    A : 3,
-    B : 4,
-    C : 5,
-    D : 8
+type SymbolCount = {
+    symbol: string;
+    count: number;
 }
+
+const SYMBOL_COUNTS: SymbolCount[] = [
+    { symbol: "🍒", count: 8},
+    { symbol: "🍋", count: 5},
+    { symbol: "🍌", count: 4},
+    { symbol: "💎", count: 3},
+];
+
+
 
 //function for getting deposits
 const getDeposit = (): number => {
@@ -80,12 +87,49 @@ const getBet = (rows: number): number => {
     }
 }
 
+//function for generating a slot machine spin.
+const spinSlots = () => {
+    //populate array of possible symbols
+    const symbols: string[] = [];
+    for (const item of SYMBOL_COUNTS) {
+        for (let i = 0; i < item.count; i++) {
+            symbols.push(item.symbol);
+        }
+    }
+    
+    //DEBUG: console.log(symbols)
+
+    const result: string[][] = []
+    for (let i: number = 0; i < ROWS; i++) {
+        const row: string[] = [];
+        for (let j: number = 0; j < COLS; j++) {
+            if (symbols.length === 0) break;
+            const index: number = Math.floor(Math.random()* symbols.length)
+            const removed: string[] = symbols.splice(index, 1);
+            const value: string = removed[0]!
+
+            if (value !== undefined) {
+                row.push(value);
+            }
+            
+            //DEBUG: console.log(`Populated [${i}][${j}]`);
+        }
+        result.push(row);
+    }
+
+    return result;
+}
+
 let balance: number = 0;
 
 const deposit: number = getDeposit();
 const rows: number = getRows();
 const bet: number = getBet(rows);
+const spinResult: string[][] = spinSlots();
 
 console.log(`You deposited ${deposit}`);
 console.log(`You are betting ${bet} on ${rows} rows`);
 console.log (`You have a balance of ${balance}`)
+for (let i = 0; i < spinResult.length; i++) {
+    console.log(spinResult[i])
+}
